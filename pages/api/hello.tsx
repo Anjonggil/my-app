d6714214680e142003f07a9e651c35a0fc4a278d
ANJONGGIL <qws458@naver.com>
2023-05-15 08:20:45 +0900

    next js api 시작

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,18 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { type } from "os";
-
-class Data {
-    name : string;
-
-    constructor(name: string){
-        this.name = name;
-    }
-};
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ){
-    let result = new Data("giri An");
-    res.status(200).json(result);
+    res.status(200).json({ pokemon : {name: "안종길"}});
 }
